a9e142e7c350b168e3f127ddcf94d5ab22672f75
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-07-01 00:11:11 +0200

    add custom helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title of the page with a definable prefix
+  def full_title(page_title = '')
+    base_title = 'LearnEnough'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
