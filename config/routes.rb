e270a867bf21a7338207320a798195d074535c00
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-06-30 19:15:24 +0200

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'application#ping'
 end
